21cf71ebbeca7b026530a6e641f4ed7c2876f2a2
Kieran Lockyer <kieran.lockyer.87@gmail.com>
2019-01-10 00:04:34 +1000

    some more tuts completed

---
+++ react_and_redux/poketimes/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut impedit quos repellendus eum rerum saepe sequi reiciendis, quas corrupti sint cupiditate sed nobis alias ad animi porro doloremque eveniet odit.
+        </div>
+    )
+}
+
+export default Contact
